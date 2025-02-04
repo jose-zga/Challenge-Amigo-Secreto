29f0483a8b323610c89ef6b1b542269f28df7f65
José Zaragoza <josezgav94@gmail.com>
2025-02-04 01:11:36 -0600

    Se agrega la función sortearAmigo

--- Challenge Amigo Secreto/app.js
+++ Challenge Amigo Secreto/app.js
@@ -4,34 +4,45 @@
 
 let amigos = [];
 
-//Esta función corrobora si la información proporcionada está vacia, si es así, arroja una alerta, si hay un nombre correcto lo agrega a la lista "amigos"
+//corrobora si la información proporcionada es un nombre correcto y lo agrega a la lista "amigos"
 function agregarAmigo(){
     
-    let inputNombre = document.getElementById("amigo").value;
+    let inputNombre = document.getElementById("amigo").value; //obtiene el valor ingresado
 
-    if(inputNombre == ""){
-        alert("Escriba un nombre")
+    if(inputNombre == ""){ //comprueba que no sea un dato vacio
+        alert("Escriba un nombre valido ")//si está vacio arroja una alerta
     }else{
-        amigos.push(inputNombre);
-        document.getElementById("amigo").value = "";
+        amigos.push(inputNombre); //si el nombre es valido, se agrega al array amigos
+        document.getElementById("amigo").value = ""; // limpia el campo para ingresar un nuevo nombre
         console.log(amigos);
-        actualizarListaAmigos();
+        actualizarListaAmigos(); //función para mostrar los nombres en la pantalla
     }
     
 }
 
+//Actualiza la lista de amigos
 function actualizarListaAmigos (){
-    let listaDeNombres = document.getElementById("listaAmigos");
-    listaDeNombres.innerHTML = "";
+    let listaDeNombres = document.getElementById("listaAmigos");//se obtiene la lista en la que se va a trabajar en una variable
+    listaDeNombres.innerHTML = ""; //borra la lista, por si quedó un nombre al actualizar
 
-    for (let i = 0; i < amigos.length; i++) {
-        let item = document.createElement("li");
-        item.textContent = amigos[i];
-        listaDeNombres.appendChild(item);
+    for (let i = 0; i < amigos.length; i++) { //Es la forma de recorrer un array
+        let item = document.createElement("li"); // con este metodo se crea un elemento de tipo <li>
+        item.textContent = amigos[i]; //se asigna el contenido al elemento <li> creado
+        listaDeNombres.appendChild(item); //Con este metodo se agrega el elemento <li> al elemento HTML <ul>
     }
 }
 
+// implementa función para sortear amigos
+function sortearAmigo (){
+    if(amigos.length <= 0){
+        alert("Ingresa los nombres de los participantes"); // alerta si no hay datos en la lista
+    }else{
+        let numeroAleatorio = Math.floor(Math.random() * amigos.length);// genera un numero aleatorio
+        let busquedaEnAmigos = amigos[numeroAleatorio]; //localiza el nombre que corresponde al indice  del numero aleatorio
+        document.getElementById("listaAmigos").style.display = "none";
+        let amigoSecreto = document.getElementById("resultado"); //Se obtiene el valor de resultado del HTML
+        amigoSecreto.innerHTML = `El amigo secreto es ${busquedaEnAmigos}`; //se muestra el mensaje con el amigo secreto
 
+    }
+}
 
-
-agregarAmigo();
