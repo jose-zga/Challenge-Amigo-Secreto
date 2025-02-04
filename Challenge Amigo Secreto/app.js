4e9e803d3f3642a89142bdb0e5d9c4d3110a88c5
José Zaragoza <josezgav94@gmail.com>
2025-02-03 22:06:37 -0600

    Implementa función agregarAmigo al JS

--- Challenge Amigo Secreto/app.js
+++ Challenge Amigo Secreto/app.js
@@ -1,1 +1,26 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus
+//  habilidades en lógica de programación.
+// Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+//Esta función corrobora si la información proporcionada está vacia, si es así, arroja una alerta, si hay un nombre correcto lo agrega a la lista "amigos"
+function agregarAmigo(){
+    
+    let inputNombre = document.getElementById("amigo").value;
+
+    if(inputNombre == ""){
+        alert("Escriba un nombre")
+    }else{
+        amigos.push(inputNombre);
+        document.getElementById("amigo").value = "";
+        console.log(amigos);
+    }
+    
+}
+
+
+agregarAmigo();
+
+
+    
